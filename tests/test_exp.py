ec13306c08ef4fdb1b93af0391d2b3aba9df3ff6
bllu404 <96asdffdsa69@protonmail.com>
2022-07-26 14:36:15 +0300

    split tests into lower and upper ranges with different precision

--- tests/test_exp.py
+++ tests/test_exp.py
@@ -1,7 +1,15 @@
+# A note on precision
+# ----------------------------
+# The larger the exponent, the less precise the cairo `exp` function becomes.
+# This is because calculating the result for larger exponents involves more fixed-point multiplications,
+# each of create additional precision loss.
+# As a result, most of the tests have been split into two: one checking the upper range with larger error margins,
+# and one checking the lower range with a smaller error margin.
+
 import os
 import asyncio
 from decimal import Decimal
-from math import exp
+from numpy import exp
 from typing import Union 
 
 import pytest
@@ -53,16 +61,15 @@ def compile_contract(rel_contract_path: str) -> ContractClass:
         cairo_path=[tld, os.path.join(tld, "contracts", "lib")],
     )
 
-# Python's exponential function starts to diverge significantly from the Cairo implementation
-# for exponents larger than around 25-26.
-# Interestingly enough, when checking against high-precision online calculators,
-# it seems that the Cairo version is actually significantly
-# more precise than the python version for large numbers, whereas they tend to
-# be extremely close to each other for smaller numbers.
-st_non_divergent_range = st.integers(min_value=-to_wad(40), max_value=to_wad(25))
+
+# Lower bound of the upper range
+UPPER_RANGE_LB = 10
+st_upper_range = st.integers(min_value=-to_wad(UPPER_RANGE_LB), max_value=to_wad(40))
+st_lower_range = st.integers(min_value=-to_wad(40), max_value=to_wad(UPPER_RANGE_LB))
 
 st_invalid_range1 = st.integers(min_value=-(2**125), max_value=-to_wad(40) - 1)
 st_invalid_range2 = st.integers(min_value=to_wad(40) + 1, max_value=2**125)
+
 
 
 @pytest.fixture(scope="session")
@@ -80,29 +87,7 @@ async def deploy_test_contract():
     return contract
 
 
-@settings(max_examples=100, deadline=None)
-@given(val=st_non_divergent_range)
-@example(val=to_wad(25))
-@example(val=-to_wad(40))
-@pytest.mark.asyncio
-async def test_exp_pass(deploy_test_contract, val):
-    contract = deploy_test_contract
-
-    # Python's exponential function starts to diverge significantly from the Cairo implementation
-    # for exponents larger than around 25-26.
-    # Interestingly enough, when checking against high-precision online calculators,
-    # it seems that the Cairo version is actually significantly
-    # more precise than the python version for large numbers, whereas they tend to
-    # be extremely close to each other for smaller numbers.
-
-    tx = await contract.get_exp(signed_int_to_felt(val)).invoke()
-
-    result = Decimal(from_wad(tx.result.res))
-    expected_result = Decimal(exp(from_wad(val)))
-    assert_equalish(result, expected_result, Decimal("0.0001"))
-
-
-@settings(max_examples=100, deadline=None)
+@settings(max_examples=10, deadline=None)
 @given(val_r1=st_invalid_range1, val_r2=st_invalid_range2)
 @example(val_r1=-to_wad(40) - 1, val_r2=to_wad(40) + 1)
 @pytest.mark.asyncio
@@ -116,13 +101,33 @@ async def test_exp_fail(deploy_test_contract, val_r1, val_r2):
         await contract.get_exp(signed_int_to_felt(val_r2)).invoke()
 
 
-# This tests that exp(-x) = 1/exp(x)
-@settings(max_examples=100, deadline=None)
-@given(val=st_non_divergent_range)
+#
+# Lower range
+#
+
+
+@settings(max_examples=50, deadline=None)
+@given(val=st_lower_range)
 @example(val=to_wad(25))
 @example(val=-to_wad(40))
 @pytest.mark.asyncio
-async def test_exp_inversions(deploy_test_contract, val):
+async def test_exp_pass_lower(deploy_test_contract, val):
+    contract = deploy_test_contract
+
+    tx = await contract.get_exp(signed_int_to_felt(val)).invoke()
+
+    result = Decimal(from_wad(tx.result.res))
+    expected_result = Decimal(exp(from_wad(val)))
+    assert_equalish(result, expected_result, Decimal("0.0001"))
+
+
+# This tests that exp(-x) = 1/exp(x)
+@settings(max_examples=50, deadline=None)
+@given(val=st_lower_range)
+@example(val=to_wad(25))
+@example(val=-to_wad(40))
+@pytest.mark.asyncio
+async def test_exp_inversions_lower(deploy_test_contract, val):
     contract = deploy_test_contract
 
     result = Decimal(from_wad((await contract.get_exp(signed_int_to_felt(val)).invoke()).result.res))
@@ -133,12 +138,13 @@ async def test_exp_inversions(deploy_test_contract, val):
 
 
 # This tests that exp(x+y) = exp(x)*exp(y)
-@settings(max_examples=100, deadline=None)
-@given(val1=st_non_divergent_range, val2=st_non_divergent_range)
+@settings(max_examples=50, deadline=None)
+@given(val1=st_lower_range, val2=st_lower_range)
 @pytest.mark.asyncio
-async def test_exp_sum(deploy_test_contract, val1, val2):
+async def test_exp_sum_lower(deploy_test_contract, val1, val2):
 
-    # Skip if the sum of the two values is greater than 40 or less than -40 (the maximum and minimum possible values respectively)
+    # Skip if the sum of the two values is greater than 40 or less than -40
+    # (the maximum and minimum possible values respectively)
     assume(val1 + val2 <= to_wad(40) and val1 + val2 >= -to_wad(40))
 
     contract = deploy_test_contract
@@ -147,4 +153,59 @@ async def test_exp_sum(deploy_test_contract, val1, val2):
     result2 = Decimal(from_wad((await contract.get_exp(signed_int_to_felt(val2)).invoke()).result.res))
     result_sum = Decimal(from_wad((await contract.get_exp(signed_int_to_felt(val1 + val2)).invoke()).result.res))
 
-    assert_equalish(result1 * result2, result_sum, Decimal("0.01"))
+    assert_equalish(result1 * result2, result_sum, Decimal("0.001"))
+
+
+#
+# Upper range
+#
+
+
+@settings(max_examples=50, deadline=None)
+@given(val=st_upper_range)
+@example(val=to_wad(25))
+@example(val=-to_wad(40))
+@pytest.mark.asyncio
+async def test_exp_pass_upper(deploy_test_contract, val):
+    contract = deploy_test_contract
+
+    tx = await contract.get_exp(signed_int_to_felt(val)).invoke()
+
+    result = Decimal(from_wad(tx.result.res))
+    expected_result = Decimal(exp(from_wad(val)))
+    assert_equalish(result, expected_result, Decimal("1"))
+
+
+# This tests that exp(-x) * exp(x) = 1
+@settings(max_examples=50, deadline=None)
+@given(val=st_upper_range)
+@example(val=to_wad(40))
+@example(val=-to_wad(40))
+@pytest.mark.asyncio
+async def test_exp_inversions_upper(deploy_test_contract, val):
+    contract = deploy_test_contract
+
+    result = Decimal(from_wad((await contract.get_exp(signed_int_to_felt(val)).invoke()).result.res))
+    inverse_result = Decimal(from_wad((await contract.get_exp(signed_int_to_felt(-val)).invoke()).result.res))
+
+    # Precision starts getting pretty bad with all these multiplications and divisions
+    assert_equalish(result * inverse_result, Decimal(1), Decimal("0.3"))
+
+
+# This tests that exp(x+y) = exp(x)*exp(y)
+@settings(max_examples=50, deadline=None)
+@given(val1=st_upper_range, val2=st_upper_range)
+@pytest.mark.asyncio
+async def test_exp_sum_upper(deploy_test_contract, val1, val2):
+
+    # Skip if the sum of the two values is greater than 40 or less than -40
+    # (the maximum and minimum possible values respectively)
+    assume(val1 + val2 <= to_wad(40) and val1 + val2 >= -to_wad(40))
+
+    contract = deploy_test_contract
+
+    result1 = Decimal(from_wad((await contract.get_exp(signed_int_to_felt(val1)).invoke()).result.res))
+    result2 = Decimal(from_wad((await contract.get_exp(signed_int_to_felt(val2)).invoke()).result.res))
+    result_sum = Decimal(from_wad((await contract.get_exp(signed_int_to_felt(val1 + val2)).invoke()).result.res))
+
+    assert_equalish(result1 * result2, result_sum, Decimal("1"))
